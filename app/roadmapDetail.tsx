efb1077f386f55f50836b9d9fc8df8339b089478
Shubhamghude808 <112695994+Shubhamghude808@users.noreply.github.com>
2026-03-27 16:06:51 +0530

    Image Zoom and swipe down gestures

--- app/roadmapDetail.tsx
+++ app/roadmapDetail.tsx
@@ -23,12 +23,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  image: {
-    width: "100%",
-    height: "100%",
-    resizeMode: "contain",
   },
 });
